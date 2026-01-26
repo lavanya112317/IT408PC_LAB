cff194b8ce2fa30ac8088d139acbfde86359bd4e
lavanya112317 <patlollalavanya345@gmail.com>
2026-01-26 21:33:29 +0530

    Create L1P3.java

---
+++ LAB1/L1P3.java
@@ -0,0 +1,13 @@
+class L1P3{
+    public static void main(String[] args){
+        char ch = 'a';
+        int x = 10;
+        short s = 30;
+        long l = 456;
+        float fl = 30f;
+        double d = x;
+        byte z = 12;
+        boolean b = false;
+        System.out.println("Integer value : " + x + " short: "+ s + " long: "+ l + " float: " + fl + " double: " + d+ " Byte: " + z+" Boolean: "+ b);
+    }
+}
