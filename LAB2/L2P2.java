1b0f5420fc0a50c0588e225258d33e7c82e7dc44
lavanya112317 <patlollalavanya345@gmail.com>
2026-02-12 23:15:45 +0530

    Update L2P2.java

--- LAB2/L2P2.java
+++ LAB2/L2P2.java
@@ -6,18 +6,16 @@ class L2P2{
 	int i=1;
 	Outerwhile: while(i<=n){ 
 	               for(int j=1;j<=10;j++){
-			System.out.println(i+"x"+j+"="+i*j);
+			System.out.println(n+"x"+j+"="+n*j);
 			if (j>=10){
 			System.out.println("If you want to continue please enter 1 otherwise 0");
 			choice=sc.nextInt();
 			switch(choice){
 				case 0: break Outerwhile;
-				case 1: continue Outerwhile;
+				case 1:i++;
+					   continue Outerwhile;
 				}
 			}
-			i++;
-        
-
 			}
 		}
 	}
